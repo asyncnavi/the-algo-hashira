053bedab8678a029ed64004a5069bc12f908e374
Navraj Sandhu <asyncnavi@gmail.com>
2024-01-05 15:54:58 +0530

    day:1[two_sum]

---
+++ two_sum/src/main.rs
@@ -0,0 +1,22 @@
+use std::collections::HashMap;
+
+fn two_sum(nums: Vec<i32>, target: i32) -> Vec<i32> {
+    let mut num_map: HashMap<i32, usize> = HashMap::new();
+
+    for (i, &num) in nums.iter().enumerate() {
+        let complement = target - num;
+        if let Some(&index) = num_map.get(&complement) {
+            return vec![index as i32, i as i32];
+        }
+        num_map.insert(num, i);
+    }
+
+    vec![]
+}
+
+fn main() {
+    let nums = vec![2, 7, 11, 15];
+    let target = 9;
+    let result = two_sum(nums, target);
+    println!("Indices: {:?}", result);
+}
