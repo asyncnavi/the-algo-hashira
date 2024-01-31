7910a14d28724c66dc47c6183bd0650ab8385a96
Navraj Sandhu <asyncnavi@gmail.com>
2024-01-31 22:29:23 +0530

    add array reveser method

--- arrays/src/main.rs
+++ arrays/src/main.rs
@@ -5,14 +5,9 @@ mod operation;
 
 
 fn main() {
-    let values: [isize;7] = [12,16,18,22,46,78,50];
-    let mut input_line = String::new();
-    io::stdin().read_line(&mut input_line).expect("Failed to read input");
-    let key = input_line.trim().parse().expect("Input is not an integer");
-
-    match search::search::binary_search_rec(&values, key) {
-        Some(index) => println!("{key} is found at index {index}"),
-        None => println!("{key} is not found.")
-    }
+    let mut values: [isize;7] = [12,16,18,22,46,78,50];
+    println!("Array before rev ... : {:?}", values);
+    operation::operation::rev(&mut values);
+    println!("Array after rev ... : {:?}", values);
 
 }
--- arrays/src/operation.rs
+++ arrays/src/operation.rs
@@ -1,7 +1,7 @@
 pub mod operation {
     pub fn sum(arr : &[isize]) -> isize {
         let mut sum = 0;
-        for i in  arr[..] {
+        for i in  0..arr.len() {
             sum += arr[i];
         }
         sum
@@ -9,7 +9,7 @@ pub mod operation {
 
     pub fn max(arr : &[isize]) -> isize {
        let mut max =  arr[0];
-       for i in arr[1..] {
+       for i in 1..arr.len() {
             if (arr[i] > max) {
                  max = arr[i];
             }
@@ -20,10 +20,20 @@ pub mod operation {
 
     pub fn avg(arr : &[isize]) -> isize {
         let mut total  = 0;
-        for i in arr[..] {
+        for i in 0..arr.len() {
             total += arr[i];
         }
 
         total / arr.len() as isize
     }
+
+    pub fn rev<T>(arr: &mut [T]) {
+        let len = arr.len();
+        let mid = len /2 ;
+
+        for i in 0..mid {
+            arr.swap(i, len - 1 -i);
+        }
+    }
+
 }
