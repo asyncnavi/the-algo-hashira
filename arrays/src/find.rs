4868bc40919be1969fa455ff5e8efef7fe4d8fe7
Navraj Sandhu <asyncnavi@gmail.com>
2024-02-04 13:25:05 +0530

    add find missing using map

--- arrays/src/find.rs
+++ arrays/src/find.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+use crate::operation;
+
 pub fn single_missing(a: &[isize]) -> Option<isize> {
     let diff = a[0] - 0;
 
@@ -20,6 +23,26 @@ pub fn multiple_missing(a : &[isize]) -> Vec<isize>{
                 res.push(i as isize +diff);
                 diff+=1;
             }
+        }
+    }
+
+    res
+}
+
+
+pub fn multiple_missing_using_hash(a: &[isize]) -> Vec<isize> {
+    let mut indexes: HashMap<isize, bool> = HashMap::new();
+    let mut res = Vec::new();
+
+    for &element in a {
+        indexes.insert(element, true);
+    }
+
+    let max = operation::max(a);
+    let min = a[0];
+    for i in min..max {
+        if !indexes.contains_key(&i) {
+            res.push(i)
         }
     }
 
--- arrays/src/main.rs
+++ arrays/src/main.rs
@@ -8,7 +8,7 @@ mod find;
 fn main() {
     let arr: [isize;11] = [6,7,8,9,11,12,15,16,17,18,19];
 
-    let result = find::multiple_missing(&arr);
+    let result = find::multiple_missing_using_hash(&arr);
 
     println!("{:?}", result);
 }
